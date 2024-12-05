4621e00fec0ad93d173ea76f25b70cf478494beb
jephter-olamiposi <grandbusta@gmail.com>
2024-12-05 02:03:23 +0100

    Specify the calculator account structure

--- programs/calculator_dapp/src/lib.rs
+++ programs/calculator_dapp/src/lib.rs
@@ -4,13 +4,28 @@ declare_id!("4LSZiXMKLkGeAmX6NMwWmoNn3Nna19HMT6i8iuxo8c76");
 
 #[program]
 pub mod calculator_dapp {
-    use super::*;
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    use super::*;
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 256)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Calculator {
+    pub greeting: String,
+    pub result: i32,
+    pub remainder: i32,
+}
